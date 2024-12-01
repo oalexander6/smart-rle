a4a587175fc5ded847543635510703df6bd336dc
Owen Alexander <owen6alexander@gmail.com>
2024-11-30 23:18:40 -0500

    Implement encoder

--- rle.go
+++ rle.go
@@ -2,16 +2,48 @@ package rle
 
 import (
 	"errors"
-	"strconv"
+	"fmt"
+	"math"
+	"slices"
 )
 
 var (
 	ErrDelimitterFound = errors.New("delimitter value found in the input")
 	ErrMalformedInput  = errors.New("malformed input")
+	ErrInvalidBase     = errors.New("must supply base between 2-62")
 )
 
 func Encode(input []byte, delim byte) ([]byte, error) {
-	return []byte{}, nil
+	result := make([]byte, 0)
+
+	if len(input) == 0 {
+		return result, nil
+	}
+
+	last := input[0]
+	count := 1
+
+	for i := 1; i < len(input); i++ {
+		if input[i] == last {
+			count++
+			continue
+		}
+
+		base62Val := ToBase62(count)
+
+		if len(base62Val)+2 < count {
+			result = append(result, fmt.Sprintf("%c%s%c%c", delim, base62Val, delim, last)...)
+		} else {
+			result = append(result, makeBytes(count, last)...)
+		}
+
+		count = 1
+		last = input[i]
+	}
+
+	result = append(result, makeBytes(count, last)...)
+
+	return result, nil
 }
 
 func Decode(input []byte, delim byte) ([]byte, error) {
@@ -64,13 +96,12 @@ func getNextRunLength(input []byte, delim byte) (int, int, error) {
 		return 0, 0, ErrMalformedInput
 	}
 
-	length, err := strconv.Atoi(string(lengthBytes))
+	length, err := FromBase62(string(lengthBytes))
 	if err != nil {
-		return 0, 0, ErrMalformedInput
+		return 0, 0, err
 	}
 
 	bytesRead := 2 + len(lengthBytes)
-
 	return length, bytesRead, nil
 }
 
@@ -80,4 +111,44 @@ func makeBytes(n int, val byte) []byte {
 		result[i] = val
 	}
 	return result
+}
+
+var base62Digits = []byte{
+	'0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+	'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
+	'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
+	'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D',
+	'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N',
+	'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X',
+	'Y', 'Z',
+}
+
+func ToBase62(val int) string {
+	// number of digits to represent a number in base 'b' is logb(number) + 1
+	numDigits := int(math.Log10(float64(val))/math.Log10(float64(62))) + 1
+
+	result := make([]byte, numDigits)
+	remaining := val
+	i := 0
+	for remaining > 0 {
+		result[numDigits-i-1] = base62Digits[remaining%62]
+		remaining = remaining / 62
+		i += 1
+	}
+
+	return string(result)
+}
+
+func FromBase62(val string) (int, error) {
+	result := 0
+
+	for i := 0; i < len(val); i++ {
+		curr := slices.Index(base62Digits, val[i])
+		if curr == -1 {
+			return 0, ErrMalformedInput
+		}
+		result += int(math.Pow(float64(62), float64(len(val)-i-1))) * curr
+	}
+
+	return result, nil
 }
--- rle_test.go
+++ rle_test.go
@@ -1,41 +1,98 @@
-package rle_test
+package rle
 
 import (
 	"bytes"
 	"errors"
 	"testing"
-
-	rle "github.com/oalexander6/smart-rle"
 )
 
 func TestDecode(t *testing.T) {
 	input := []byte("as3df")
-	result, err := rle.Decode(input, '.')
+	result, err := Decode(input, '.')
 	if err != nil || !bytes.Equal(result, input) {
 		t.FailNow()
 	}
 
 	input = []byte("as.3.df")
-	result, err = rle.Decode(input, '.')
+	result, err = Decode(input, '.')
 	if err != nil || !bytes.Equal(result, []byte("asdddf")) {
 		t.FailNow()
 	}
 
+	input = []byte("as.12.df")
+	result, err = Decode(input, '.')
+	if err != nil || !bytes.Equal(result, []byte("asddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddf")) {
+		t.FailNow()
+	}
+
 	input = []byte("as.3.d.4.f")
-	result, err = rle.Decode(input, '.')
+	result, err = Decode(input, '.')
 	if err != nil || !bytes.Equal(result, []byte("asdddffff")) {
 		t.FailNow()
 	}
 
 	input = []byte("as.3df")
-	_, err = rle.Decode(input, '.')
-	if !errors.Is(err, rle.ErrMalformedInput) {
+	_, err = Decode(input, '.')
+	if !errors.Is(err, ErrMalformedInput) {
 		t.FailNow()
 	}
 
-	input = []byte("as.3a.df")
-	_, err = rle.Decode(input, '.')
-	if !errors.Is(err, rle.ErrMalformedInput) {
+	input = []byte("as.3?.df")
+	_, err = Decode(input, '.')
+	if !errors.Is(err, ErrMalformedInput) {
+		t.FailNow()
+	}
+}
+
+func TestEncode(t *testing.T) {
+	result, err := Encode([]byte("asdf"), '.')
+	if err != nil || !bytes.Equal(result, []byte("asdf")) {
+		t.FailNow()
+	}
+
+	result, err = Encode([]byte("asddf"), '.')
+	if err != nil || !bytes.Equal(result, []byte("asddf")) {
+		t.FailNow()
+	}
+
+	result, err = Encode([]byte("asdddf"), '.')
+	if err != nil || !bytes.Equal(result, []byte("asdddf")) {
+		t.FailNow()
+	}
+
+	result, err = Encode([]byte("asddddf"), '.')
+	if err != nil || !bytes.Equal(result, []byte("as.4.df")) {
+		t.FailNow()
+	}
+}
+
+func TestToBase82(t *testing.T) {
+	result := ToBase62(12)
+	if result != "c" {
+		t.FailNow()
+	}
+
+	result = ToBase62(94)
+	if result != "1w" {
+		t.FailNow()
+	}
+
+	result = ToBase62(12478)
+	if result != "3fg" {
+		t.FailNow()
+	}
+}
+
+func TestFromBase62(t *testing.T) {
+	if result, err := FromBase62("c"); err != nil || result != 12 {
+		t.FailNow()
+	}
+
+	if result, err := FromBase62("1w"); err != nil || result != 94 {
+		t.FailNow()
+	}
+
+	if result, err := FromBase62("3fg"); err != nil || result != 12478 {
 		t.FailNow()
 	}
 }
